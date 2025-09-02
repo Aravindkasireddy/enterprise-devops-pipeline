beaa7d840eb09da10a5ef8d6bcd6feadc87d093a
Aravind Reddy Kasireddy <122817245+Aravindkasireddy@users.noreply.github.com>
2025-09-02 09:03:08 -0500

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,17 @@
-output "cluster_id" {
-  value = module.eks.cluster_id
+# --- ECR Outputs ---
+output "ecr_repository_url" {
+  value = aws_ecr_repository.flask_app.repository_url
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
-}
+# --- EKS Outputs (disabled until cluster is enabled) ---
+# output "cluster_id" {
+#   value = module.eks.cluster_id
+# }
+#
+# output "cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
+#
+# output "cluster_security_group_id" {
+#   value = module.eks.cluster_security_group_id
+# }
